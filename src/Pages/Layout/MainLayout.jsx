cc0a929de9ef2742f8514b168c1a270f7f8330cf
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-08-26 23:23:24 +0600

    integrate font and gradient bg

--- src/Pages/Layout/MainLayout.jsx
+++ src/Pages/Layout/MainLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../Shared/Header';
-import Footer from '../Shared/Footer';
+import { Outlet } from "react-router-dom";
+import Header from "../Shared/Header";
+import Footer from "../Shared/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className='max-w-7xl mx-auto'>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );  
+  return (
+    <div className="mx-auto font-inter">
+      <main className="container w-[90%] mx-auto">
+        <Header />
+        <Outlet />
+        <Footer />
+      </main>
+    </div>
+  );
 };
 
 export default MainLayout;
