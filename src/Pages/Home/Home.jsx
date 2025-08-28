3cb399a7aee7d02fc14d20df0c58814049774911
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-08-28 10:41:35 +0600

    added image div in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import img1 from "../../assets/profile1.jpg"
+import img2 from "../../assets/profile2.jpg"
+import img3 from "../../assets/profile3.jpg"
+import ParticlesComponent from '../../Components/particle-animation/ParticlesComponent';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
-        </div>
+        <main>
+            <ParticlesComponent></ParticlesComponent>
+            <div>
+                <div>
+                    {/* profile image box */}
+                    <div className="flex justify-center lg:justify-start">
+                        <img className="rounded-full w-[260px] h-[260px] ring-4 shadow-2xl shadow-[#ffffff7d] lg:ring-6 ring-[#00a6fb] object-cover" src={img1} alt="Shahruk" />
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 };
 
